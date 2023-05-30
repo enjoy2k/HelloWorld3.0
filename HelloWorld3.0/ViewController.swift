4881830a269dc37cf3aa2b3eb66dfcd48aa8594a
Daniil Kozlov <enjoy2k@mail.ru>
2023-05-30 17:15:08 +0300

    Написал логику. Парюсь с размером шрифта кнопки

--- HelloWorld3.0/ViewController.swift
+++ HelloWorld3.0/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    @IBOutlet weak var showTextButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorldLabel.isHidden = true
+        helloWorldLabel.textColor = .systemRed
+        showTextButton.layer.cornerRadius = 10
+        showTextButton.titleLabel?.font = UIFont.systemFont(ofSize: 25)
     }
-
-
+    @IBAction func showTextButtonPressed() {
+        helloWorldLabel.isHidden.toggle()
+        
+        if helloWorldLabel.isHidden {
+            showTextButton.setTitle("Show Text", for: .normal)
+            
+        } else {
+            showTextButton.setTitle("Hide Text", for: .normal)
+        }
+    }
 }
 
